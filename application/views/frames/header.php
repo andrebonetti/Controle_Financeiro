eabce0ede1c4a1cb58fa3bfc82d3055ad0299bb3
Andre Bonetti <andrebonetti2@gmail.com>
2017-12-05 15:10:05 -0200

    Erro Bootstrap 4.0

--- application/views/frames/header.php
+++ application/views/frames/header.php
@@ -8,15 +8,17 @@
         
         <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro' rel='stylesheet' type='text/css'>
              
-        <link rel="shortcut icon" type="image/x-icon" href="<?=base_url("img/Shortcut.png")?>" title="" >
+        <link rel="shortcut icon" type="image/x-icon" href=">" title="">
         <title> Controle Financeiro </title>
                       
-        <link rel="stylesheet" type="text/css" href="<?=base_url("css/bootstrap.css")?>">     
+        <link rel="stylesheet" type="text/css" href="<?=base_url("libs/css/bootstrap.css")?>">     
         <link rel="stylesheet/less" href="<?=base_url("less/style.less")?>">    
         
-        <script src="<?=base_url("js/jquery-2.1.3.min.js")?>"></script>
-        <script src="<?=base_url("js/bootstrap.js")?>"></script>
-        <script src="<?=base_url("js/less.js")?>"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.6/popper.min.js"></script>
+        <script src="<?=base_url("libs/js/jquery-2.1.3.min.js")?>"></script>
+        <script src="<?=base_url("libs/js/bootstrap.js")?>"></script>
+        <script src="<?=base_url("libs/js/less.js")?>"></script>
+
                	    	
 	</head>
 
